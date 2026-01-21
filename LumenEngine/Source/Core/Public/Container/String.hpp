414321e6f30e20d760704407d5ea60fb60af8eb1
Leorevoir <leo.quinzler@epitech.eu>
2026-01-21 18:16:58 +0100

    Chore(Core): Temp add String, need to make a custom one

---
+++ LumenEngine/Source/Core/Public/Container/String.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file String.hpp
+ * @brief Declaration of the FString class for string manipulation.
+ */
+
+#pragma once
+
+// TODO: own implementation of FString
+#include <string>
+
+namespace LumenEngine
+{
+
+// TODO: own implementation of FString
+class FString : public std::string
+{
+public:
+
+private:
+};
+
+} // namespace LumenEngine
