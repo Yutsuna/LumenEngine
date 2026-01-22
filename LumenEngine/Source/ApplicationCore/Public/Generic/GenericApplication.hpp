b22c0e9d4359283b7c66eafb31143dda32bf85b6
Leorevoir <leo.quinzler@epitech.eu>
2026-01-22 20:59:51 +0100

    Chore(Core): Add GenericApplicationMessageHandler definitions

---
+++ LumenEngine/Source/ApplicationCore/Public/Generic/GenericApplication.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file GenericApplication.hpp
+ * @brief Declaration of the FGenericApplication class for cross-platform application management.
+ */
+
+#pragma once
+
+#include "Container/SharedPtr.hpp"
+
+namespace LumenEngine
+{
+
+/**
+ * Forwards
+ */
+
+class FGenericWindow;
+struct FGenericWindowDescription;
+class FGenericApplicationMessageHandler;
+class ICursor;
+
+/**
+ * @class FGenericApplication
+ * @brief A base class for platform-independent application management.
+ * @details Provides a common interface for handling application-level functionality
+ */
+class FGenericApplication
+{
+public:
+
+    FGenericApplication ();
+    virtual ~FGenericApplication () = default;
+
+    /** Sets the message handler for the application */
+    virtual void SetMessageHandler ( const TSharedRef<FGenericApplicationMessageHandler> &InMessageHandler );
+
+    /** Retrieves the current message handler of the application */
+    TSharedPtr<FGenericApplicationMessageHandler> GetMessageHandler () const;
+
+    /** Polls and processes messages for the application */
+    virtual void PumpMessages ( const Float32 DeltaTime );
+
+    /** Sets the cursor for the application */
+    virtual void SetCursor ( const TSharedPtr<ICursor> &InCursor );
+
+    /** Retrieves the current cursor of the application */
+    TSharedPtr<ICursor> GetCursor () const;
+
+    /** Creates a new window */
+    virtual TSharedRef<FGenericWindow> MakeWindow ();
+
+    /** Initializes a window with the given parameters */
+    virtual void InitializeWindow ( const TSharedRef<FGenericWindow> &InWindow, const TSharedRef<FGenericWindowDescription> &InDescription, const TSharedPtr<FGenericWindow> &InParentWindow, const bool bShowImmediately );
+
+protected:
+
+    TSharedPtr<FGenericApplicationMessageHandler> MessageHandler;
+    TSharedPtr<ICursor>                           Cursor;
+};
+
+} // namespace LumenEngine
