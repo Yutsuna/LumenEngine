1c1533756cbafd6dd10f7a93448350b1b4bb4f34
Leorevoir <leo.quinzler@epitech.eu>
2026-01-22 20:27:28 +0100

    Chore(Core): Temp add Vector.hpp

---
+++ LumenEngine/Source/Core/Public/Container/Vector.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file Vector.hpp
+ * @brief Declaration of the Vector container class.
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace LumenEngine
+{
+
+// TODO: Implement custom vector class
+
+template <typename Type>
+class Vector final : public std::vector<Type>
+{
+public:
+
+private:
+};
+
+} // namespace LumenEngine
