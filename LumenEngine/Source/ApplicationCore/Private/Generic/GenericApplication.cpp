ce80584f3224ec4e3f403fc6af4eef794024768b
Leorevoir <leo.quinzler@epitech.eu>
2026-01-22 20:59:13 +0100

    Chore(Core): Add GenericApplication.cpp

---
+++ LumenEngine/Source/ApplicationCore/Private/Generic/GenericApplication.cpp
@@ -0,0 +1,54 @@
+/**
+ * @file GenericApplication.cpp
+ * @brief Implementation of the FGenericApplication class for cross-platform application management.
+ */
+
+#include "Generic/GenericApplication.hpp"
+#include "Generic/GenericApplicationMessageHandler.hpp"
+#include "Generic/GenericWindow.hpp"
+#include "Generic/ICursor.hpp"
+
+/**
+ * public
+ */
+
+LumenEngine::FGenericApplication::FGenericApplication ()
+    : MessageHandler( MakeShared<FGenericApplicationMessageHandler>() ), Cursor( nullptr )
+{
+    /* __ctor__ */
+}
+
+void LumenEngine::FGenericApplication::SetMessageHandler ( const TSharedRef<FGenericApplicationMessageHandler> &InMessageHandler )
+{
+    MessageHandler = InMessageHandler;
+}
+
+LumenEngine::TSharedPtr<LumenEngine::FGenericApplicationMessageHandler> LumenEngine::FGenericApplication::GetMessageHandler () const
+{
+    return MessageHandler;
+}
+
+void LumenEngine::FGenericApplication::PumpMessages ( const Float32 DeltaTime )
+{
+    /* __empty__ */
+}
+
+void LumenEngine::FGenericApplication::SetCursor ( const TSharedPtr<ICursor> &InCursor )
+{
+    Cursor = InCursor;
+}
+
+LumenEngine::TSharedPtr<LumenEngine::ICursor> LumenEngine::FGenericApplication::GetCursor () const
+{
+    return Cursor;
+}
+
+LumenEngine::TSharedRef<LumenEngine::FGenericWindow> LumenEngine::FGenericApplication::MakeWindow ()
+{
+    return MakeShareable( new FGenericWindow() );
+}
+
+void LumenEngine::FGenericApplication::InitializeWindow ( const TSharedRef<FGenericWindow> &InWindow, const TSharedRef<FGenericWindowDescription> &InDescription, const TSharedPtr<FGenericWindow> &InParentWindow, const bool bShowImmediately )
+{
+    /* __empty__ */
+}
