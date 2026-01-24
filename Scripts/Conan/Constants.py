2acffe60750753115011c97384cf36d5e342c272
Leorevoir <leo.quinzler@epitech.eu>
2026-01-24 14:22:07 +0100

    Remove(Scripts): Conan python

--- Scripts/Conan/Constants.py
+++
@@ -1,6 +0,0 @@
-from typing import Final, List
-
-__all__ = ["LIB_PATTERNS", "HEADER_PATTERNS"]
-
-LIB_PATTERNS: Final[List[str]] = ["*.a", "*.so", "*.dylib", "*.lib"]
-HEADER_PATTERNS: Final[List[str]] = ["*.hpp", "*.h", "*.inl"]
--- Scripts/Conan/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Constants import *
-from .Dependencies import *
-from .Packager import *
-from .DependencyResolver import *
