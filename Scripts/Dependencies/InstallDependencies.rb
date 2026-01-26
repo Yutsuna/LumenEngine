6d88df43b8d1b9531e941fb04cc9f0724b3ea8fd
Leorevoir <leo.quinzler@epitech.eu>
2026-01-26 22:24:51 +0100

    Fix(Scripts): Windows SDL3 fucking .dll without include path .h  omg

--- Scripts/Dependencies/InstallDependencies.rb
+++ Scripts/Dependencies/InstallDependencies.rb
@@ -9,13 +9,23 @@ DEPENDENCIES = [
     name: 'SDL3',
     version: '3.4.0',
     sources: {
-      'windows-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0-win32-x64.zip',
-      'windows-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0-win32-arm64.zip',
+      'windows-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
+      'windows-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
       'linux-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
       'macos-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
       'macos-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz'
     },
     build_commands: {
+      'windows-x64': [
+        'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=.',
+        'cmake --build build --config Release --parallel',
+        'cmake --install build'
+      ],
+      'windows-arm64': [
+        'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=.',
+        'cmake --build build --config Release --parallel',
+        'cmake --install build'
+      ],
       'linux-x64': [
         'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_INSTALL_LIBDIR=lib',
         'cmake --build build --config Release --parallel',
