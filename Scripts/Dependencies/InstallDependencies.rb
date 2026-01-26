8ecd488d52a03500d26cb11019126a014aee31ef
Leorevoir <leo.quinzler@epitech.eu>
2026-01-26 13:53:16 +0100

    Feat(Scripts): Add build_command to the JSON freezed database

--- Scripts/Dependencies/InstallDependencies.rb
+++ Scripts/Dependencies/InstallDependencies.rb
@@ -4,23 +4,42 @@
 require_relative 'Source/Manager'
 
 
-if __FILE__ == $PROGRAM_NAME
-  dependencies = [
-    {
-      name: 'SDL3',
-      version: '3.4.0',
-      sources: {
-        'windows-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0-win32-x64.zip',
-        'windows-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0-win32-arm64.zip',
-        'linux-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
-        'macos-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
-        'macos-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz'
-      }
+DEPENDENCIES = [
+  {
+    name: 'SDL3',
+    version: '3.4.0',
+    sources: {
+      'windows-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0-win32-x64.zip',
+      'windows-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0-win32-arm64.zip',
+      'linux-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
+      'macos-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
+      'macos-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz'
+    },
+    build_commands: {
+      'linux-x64': [
+        'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_INSTALL_LIBDIR=lib',
+        'cmake --build build --config Release --parallel',
+        'cmake --install build'
+      ],
+      'macos-x64': [
+        'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_INSTALL_LIBDIR=lib',
+        'cmake --build build --config Release --parallel',
+        'cmake --install build'
+      ],
+      'macos-arm64': [
+        'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_INSTALL_LIBDIR=lib',
+        'cmake --build build --config Release --parallel',
+        'cmake --install build'
+      ]
     }
-  ]
+  }
+].freeze
+
+
+if __FILE__ == $PROGRAM_NAME
 
   manager = DependencyInstaller::DependencyManager.new(
-    dependencies: dependencies,
+    dependencies: DEPENDENCIES,
     install_path: 'Vendor',
     download_path: 'TempDownloads'
   )
