dd85b625bd2b05fe1136006f8190f5bbf6c12031
Leorevoir <leo.quinzler@epitech.eu>
2026-01-26 13:54:22 +0100

    Chore(Scripts): Update Dependency datamodel to use build_command

--- Scripts/Dependencies/Source/Dependency.rb
+++ Scripts/Dependencies/Source/Dependency.rb
@@ -6,18 +6,28 @@ module DependencyInstaller
 
 
   class Dependency
-    attr_reader :name, :version, :sources, :checksum
 
-    def initialize(name:, version:, sources:, checksum: nil)
+    attr_reader :name, :version, :sources, :checksum, :build_commands
+
+    def initialize(name:, version:, sources:, checksum: nil, build_commands: nil)
       @name = name
       @version = version
       @sources = sources
       @checksum = checksum
+      @build_commands = build_commands
     end
+
 
     def get_url_for_platform(platform)
       @sources[platform.to_s] || @sources[platform.to_sym]
     end
+
+
+    def get_build_commands_for_platform(platform)
+      return nil unless @build_commands
+      @build_commands[platform.to_s] || @build_commands[platform.to_sym]
+    end
+
 
     def filename
       url = get_url_for_platform(SystemDetector.detect_platform)
