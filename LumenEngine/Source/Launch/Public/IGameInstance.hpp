141d9d70755614babb8dcc5f827c53646458b860
Leorevoir <leo.quinzler@epitech.eu>
2026-02-19 11:22:02 +0100

    Chore(Launch): Temp add GameInstance interface

---
+++ LumenEngine/Source/Launch/Public/IGameInstance.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file IGameInstance.hpp
+ * @brief Declaration of the IGameInstance interface
+ */
+
+namespace LumenEngine
+{
+
+/**
+ * @class IGameInstance
+ * @brief Interface for the game instance
+ */
+class IGameInstance
+{
+public:
+
+    IGameInstance ()          = default;
+    virtual ~IGameInstance () = default;
+
+public:
+
+protected:
+
+private:
+};
+
+} // namespace LumenEngine
