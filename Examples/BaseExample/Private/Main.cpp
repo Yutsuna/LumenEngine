40c686957dbcf2cf9ac45fe2d662f33524e05a16
Leorevoir <leo.quinzler@epitech.eu>
2026-02-19 13:51:24 +0100

    Docs(Examples): Add base example for compilation

--- Examples/BaseExample/Private/Main.cpp
+++ Examples/BaseExample/Private/Main.cpp
@@ -1,36 +1,16 @@
-#include "Linux/LinuxApplication.hpp"
-#include "Linux/LinuxWindow.hpp"
-#include "Maths/Vector.hpp"
+#include "LaunchEngine.hpp"
+#include "LaunchEngineLoops.hpp"
 
 namespace LumenEngine
 {
 
-namespace
+class GameInstance : public FEngineLoop
 {
+public:
 
-    static inline const FGenericWindowDescription GetWindowDescription ()
-    {
-        return ( const FGenericWindowDescription ){
-            .Title        = "Lumen Engine - Linux Window",
-            .Position     = Math::FVec2i( 100, 100 ),
-            .Size         = Math::FVec2i( 1280, 720 ),
-            .WindowMode   = EWindowMode::Windowed,
-            .bIsResizable = true,
-            .bIsVisible   = true };
-    }
-
-} // namespace
+private:
+};
 
 } // namespace LumenEngine
 
-int main ( void )
-{
-    auto App          = LumenEngine::FLinuxApplication::CreateLinuxApplication();
-    auto WindowDesc   = LumenEngine::MakeShared<LumenEngine::FGenericWindowDescription>( LumenEngine::GetWindowDescription() );
-    auto Window       = App->MakeWindow();
-    auto ParentWindow = nullptr;
-
-    App->InitializeWindow( Window, WindowDesc, ParentWindow, true );
-
-    return 0;
-}
+LUMEN_ENGINE_GAME( LumenEngine::GameInstance )
