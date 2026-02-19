179948cc4c4acc360d8bcee679bbd16023035c7c
Leorevoir <leo.quinzler@epitech.eu>
2026-02-19 13:49:53 +0100

    Refacto(Parsing): Remove duplicate code

--- Builder/Parsing/BuildFileParser.cs
+++ Builder/Parsing/BuildFileParser.cs
@@ -2,181 +2,173 @@ using LumenBuilder.Model;
 using LumenBuilder.Parsing.Lexer;
 using LumenBuilder.Parsing.Parser;
 
-namespace LumenBuilder
+namespace LumenBuilder.Parsing;
+
+/// <summary>
+/// Build file pure Parser, without I/O.
+/// </summary>
+public sealed class BuildFileParser
 {
 
-    namespace Parsing
+    /// <summary>
+    /// Parses a build file source into a ModuleDescriptor.
+    /// </summary>
+    public ModuleDescriptor Parse(string ModuleName, string ModuleDir, ReadOnlySpan<char> Source)
     {
+        var Memory = new ReadOnlyMemory<char>(Source.ToArray());
+        var Lexer = new BuildLexer(Memory);
+        var Tokens = Lexer.Tokenize();
+        var Parser = new BuildParser(Tokens);
+        var Ast = Parser.Parse();
 
-        /// <summary>
-        /// Build file pure Parser, without I/O.
-        /// </summary>
-        public sealed class BuildFileParser
+        return ConvertToDescriptor(ModuleName, ModuleDir, Ast);
+    }
+
+    /// <summary>
+    /// Converts an AST ModuleNode into a ModuleDescriptor.
+    /// </summary>
+    private static ModuleDescriptor ConvertToDescriptor(string ModuleName, string ModuleDir, ModuleNode Ast)
+    {
+        ModuleType Type = ModuleType.StaticLibrary;
+        var Sources = new List<string>();
+        var PublicIncludes = new List<string>();
+        var PrivateIncludes = new List<string>();
+        var Defines = new List<string>();
+        var Dependencies = new List<string>();
+        var LinuxDeps = new List<string>();
+        var WindowsDeps = new List<string>();
+        var MacOSDeps = new List<string>();
+        var LinuxLibs = new List<string>();
+        var WindowsLibs = new List<string>();
+        var MacOSLibs = new List<string>();
+
+        for (int PropIndex = 0; PropIndex < Ast.Properties.Count; PropIndex++)
         {
+            var Prop = Ast.Properties[PropIndex];
+            string PropName = Prop.Name.ToLowerInvariant();
 
-            /// <summary>
-            /// Parses a build file source into a ModuleDescriptor.
-            /// </summary>
-            public ModuleDescriptor Parse(string ModuleName, string ModuleDir, ReadOnlySpan<char> Source)
+            switch (PropName)
             {
-                var Memory = new ReadOnlyMemory<char>(Source.ToArray());
-                var Lexer = new BuildLexer(Memory);
-                var Tokens = Lexer.Tokenize();
-                var Parser = new BuildParser(Tokens);
-                var Ast = Parser.Parse();
-
-                return ConvertToDescriptor(ModuleName, ModuleDir, Ast);
+                case "type":
+                    Type = ParseModuleType(Prop.Value);
+                    break;
+                case "sources":
+                    ExtractStrings(Prop.Value, Sources);
+                    break;
+                case "public_includes":
+                    ExtractStrings(Prop.Value, PublicIncludes);
+                    break;
+                case "private_includes":
+                    ExtractStrings(Prop.Value, PrivateIncludes);
+                    break;
+                case "defines":
+                    ExtractStrings(Prop.Value, Defines);
+                    break;
+                case "deps":
+                    ExtractIdentifiers(Prop.Value, Dependencies);
+                    break;
+                case "linux_deps":
+                    ExtractIdentifiers(Prop.Value, LinuxDeps);
+                    break;
+                case "windows_deps":
+                    ExtractIdentifiers(Prop.Value, WindowsDeps);
+                    break;
+                case "macos_deps":
+                    ExtractIdentifiers(Prop.Value, MacOSDeps);
+                    break;
+                case "linux_libs":
+                    ExtractIdentifiers(Prop.Value, LinuxLibs);
+                    break;
+                case "windows_libs":
+                    ExtractIdentifiers(Prop.Value, WindowsLibs);
+                    break;
+                case "macos_libs":
+                    ExtractIdentifiers(Prop.Value, MacOSLibs);
+                    break;
             }
-
-            /// <summary>
-            /// Converts an AST ModuleNode into a ModuleDescriptor.
-            /// </summary>
-            private static ModuleDescriptor ConvertToDescriptor(string ModuleName, string ModuleDir, ModuleNode Ast)
-            {
-                ModuleType Type = ModuleType.StaticLibrary;
-                var Sources = new List<string>();
-                var PublicIncludes = new List<string>();
-                var PrivateIncludes = new List<string>();
-                var Defines = new List<string>();
-                var Dependencies = new List<string>();
-                var LinuxDeps = new List<string>();
-                var WindowsDeps = new List<string>();
-                var MacOSDeps = new List<string>();
-                var LinuxLibs = new List<string>();
-                var WindowsLibs = new List<string>();
-                var MacOSLibs = new List<string>();
-
-                for (int PropIndex = 0; PropIndex < Ast.Properties.Count; PropIndex++)
-                {
-                    var Prop = Ast.Properties[PropIndex];
-                    string PropName = Prop.Name.ToLowerInvariant();
-
-                    switch (PropName)
-                    {
-                        case "type":
-                            Type = ParseModuleType(Prop.Value);
-                            break;
-                        case "sources":
-                            ExtractStrings(Prop.Value, Sources);
-                            break;
-                        case "public_includes":
-                            ExtractStrings(Prop.Value, PublicIncludes);
-                            break;
-                        case "private_includes":
-                            ExtractStrings(Prop.Value, PrivateIncludes);
-                            break;
-                        case "defines":
-                            ExtractStrings(Prop.Value, Defines);
-                            break;
-                        case "deps":
-                            ExtractIdentifiers(Prop.Value, Dependencies);
-                            break;
-                        case "linux_deps":
-                            ExtractIdentifiers(Prop.Value, LinuxDeps);
-                            break;
-                        case "windows_deps":
-                            ExtractIdentifiers(Prop.Value, WindowsDeps);
-                            break;
-                        case "macos_deps":
-                            ExtractIdentifiers(Prop.Value, MacOSDeps);
-                            break;
-                        case "linux_libs":
-                            ExtractIdentifiers(Prop.Value, LinuxLibs);
-                            break;
-                        case "windows_libs":
-                            ExtractIdentifiers(Prop.Value, WindowsLibs);
-                            break;
-                        case "macos_libs":
-                            ExtractIdentifiers(Prop.Value, MacOSLibs);
-                            break;
-                    }
-                }
-
-                var PlatformDeps = new PlatformDependencies(LinuxDeps, WindowsDeps, MacOSDeps);
-                var PlatformLibs = new PlatformLibraries(LinuxLibs, WindowsLibs, MacOSLibs);
-
-                return new ModuleDescriptor(
-                    ModuleName,
-                    ModuleDir,
-                    Type,
-                    Sources,
-                    PublicIncludes,
-                    PrivateIncludes,
-                    Defines,
-                    Dependencies,
-                    PlatformDeps,
-                    PlatformLibs
-                );
-            }
-
-            /// <summary>
-            /// Parses a module type from an AST node.
-            /// </summary>
-            private static ModuleType ParseModuleType(AstNode Node)
-            {
-                string Value = Node switch
-                {
-                    IdentifierNode Id => Id.Value.ToLowerInvariant(),
-                    StringNode Str => Str.Value.ToLowerInvariant(),
-                    _ => "static_library"
-                };
-
-                return Value switch
-                {
-                    "executable" => ModuleType.Executable,
-                    "shared_library" or "sharedlibrary" or "shared" => ModuleType.SharedLibrary,
-                    _ => ModuleType.StaticLibrary
-                };
-            }
-
-            /// <summary>
-            /// Extracts string values from an AST node into a target list.
-            /// </summary>
-            private static void ExtractStrings(AstNode Node, List<string> Target)
-            {
-                if (Node is ArrayNode Array)
-                {
-                    for (int ElemIndex = 0; ElemIndex < Array.Elements.Count; ElemIndex++)
-                    {
-                        if (Array.Elements[ElemIndex] is StringNode Str)
-                            Target.Add(Str.Value);
-                    }
-                }
-                else if (Node is StringNode Single)
-                {
-                    Target.Add(Single.Value);
-                }
-            }
-
-            /// <summary>
-            /// Extracts identifier values from an AST node into a target list.
-            /// </summary>
-            private static void ExtractIdentifiers(AstNode Node, List<string> Target)
-            {
-                if (Node is ArrayNode Array)
-                {
-                    for (int ElemIndex = 0; ElemIndex < Array.Elements.Count; ElemIndex++)
-                    {
-                        var Element = Array.Elements[ElemIndex];
-
-                        if (Element is IdentifierNode Id)
-                        {
-                            Target.Add(Id.Value);
-                        }
-                        else if (Element is StringNode Str)
-                        {
-                            Target.Add(Str.Value);
-                        }
-                    }
-                }
-                else if (Node is IdentifierNode SingleId)
-                {
-                    Target.Add(SingleId.Value);
-                }
-            }
-
         }
 
+        var PlatformDeps = new PlatformDependencies(LinuxDeps, WindowsDeps, MacOSDeps);
+        var PlatformLibs = new PlatformLibraries(LinuxLibs, WindowsLibs, MacOSLibs);
+
+        return new ModuleDescriptor(
+            ModuleName,
+            ModuleDir,
+            Type,
+            Sources,
+            PublicIncludes,
+            PrivateIncludes,
+            Defines,
+            Dependencies,
+            PlatformDeps,
+            PlatformLibs
+        );
+    }
+
+    /// <summary>
+    /// Parses a module type from an AST node.
+    /// </summary>
+    private static ModuleType ParseModuleType(AstNode Node)
+    {
+        string Value = Node switch
+        {
+            IdentifierNode Id => Id.Value.ToLowerInvariant(),
+            StringNode Str => Str.Value.ToLowerInvariant(),
+            _ => "static_library"
+        };
+
+        return Value switch
+        {
+            "executable" => ModuleType.Executable,
+            "shared_library" or "sharedlibrary" or "shared" => ModuleType.SharedLibrary,
+            _ => ModuleType.StaticLibrary
+        };
+    }
+
+    /// <summary>
+    /// Extracts string values from an AST node into a target list.
+    /// </summary>
+    private static void ExtractStrings(AstNode Node, List<string> Target)
+    {
+        if (Node is ArrayNode Array)
+        {
+            for (int ElemIndex = 0; ElemIndex < Array.Elements.Count; ElemIndex++)
+            {
+                if (Array.Elements[ElemIndex] is StringNode Str)
+                    Target.Add(Str.Value);
+            }
+        }
+        else if (Node is StringNode Single)
+        {
+            Target.Add(Single.Value);
+        }
+    }
+
+    /// <summary>
+    /// Extracts identifier values from an AST node into a target list.
+    /// </summary>
+    private static void ExtractIdentifiers(AstNode Node, List<string> Target)
+    {
+        if (Node is ArrayNode Array)
+        {
+            for (int ElemIndex = 0; ElemIndex < Array.Elements.Count; ElemIndex++)
+            {
+                var Element = Array.Elements[ElemIndex];
+
+                if (Element is IdentifierNode Id)
+                {
+                    Target.Add(Id.Value);
+                }
+                else if (Element is StringNode Str)
+                {
+                    Target.Add(Str.Value);
+                }
+            }
+        }
+        else if (Node is IdentifierNode SingleId)
+        {
+            Target.Add(SingleId.Value);
+        }
     }
 
 }
--- Builder/Parsing/BuildFileValidator.cs
+++ Builder/Parsing/BuildFileValidator.cs
@@ -5,175 +5,142 @@
 
 using LumenBuilder.Model;
 
-namespace LumenBuilder.Parsing
+namespace LumenBuilder.Parsing;
+
+/// <summary>
+/// Validation error result.
+/// </summary>
+public sealed record ValidationError(
+    string ModuleName,
+    string Message
+);
+
+/// <summary>
+/// Validation result containing errors if any.
+/// </summary>
+public sealed record ValidationResult(
+    bool IsValid,
+    IReadOnlyList<ValidationError> Errors
+)
+{
+    public static ValidationResult Success()
+    {
+        return new ValidationResult(true, Array.Empty<ValidationError>());
+    }
+
+    public static ValidationResult Failure(IReadOnlyList<ValidationError> Errors)
+    {
+        return new ValidationResult(false, Errors);
+    }
+}
+
+/// <summary>
+/// Pure semantic validator for module descriptors.
+/// Validates build file structure and constraints without side effects.
+/// </summary>
+public static class BuildFileValidator
 {
 
     /// <summary>
-    /// Validation error result.
+    /// Validates a single module descriptor.
     /// </summary>
-    public sealed record ValidationError(
-        string ModuleName,
-        string Message
-    );
-
-    /// <summary>
-    /// Validation result containing errors if any.
-    /// </summary>
-    public sealed record ValidationResult(
-        bool IsValid,
-        IReadOnlyList<ValidationError> Errors
-    )
+    public static ValidationResult Validate(ModuleDescriptor Module)
     {
-        public static ValidationResult Success()
+        var Errors = new List<ValidationError>();
+
+        if (string.IsNullOrWhiteSpace(Module.Name))
         {
-            return new ValidationResult(true, Array.Empty<ValidationError>());
+            Errors.Add(new ValidationError(Module.Name, "Module name cannot be empty"));
         }
 
-        public static ValidationResult Failure(IReadOnlyList<ValidationError> Errors)
+        if (string.IsNullOrWhiteSpace(Module.Directory))
         {
-            return new ValidationResult(false, Errors);
+            Errors.Add(new ValidationError(Module.Name, "Module directory cannot be empty"));
         }
+
+        if (!IsValidModuleType(Module.Type))
+        {
+            Errors.Add(new ValidationError(Module.Name, $"Invalid module type: {Module.Type}"));
+        }
+
+        if (Module.Sources.Count == 0 && Module.PublicIncludes.Count == 0)
+        {
+            Errors.Add(new ValidationError(Module.Name, "Module must have at least one source file pattern or public include"));
+        }
+
+        ValidateStringList(Module.Name, Module.Sources, "Source pattern cannot be empty", Errors);
+        ValidateStringList(Module.Name, Module.PublicIncludes, "Public include path cannot be empty", Errors);
+        ValidateStringList(Module.Name, Module.PrivateIncludes, "Private include path cannot be empty", Errors);
+        ValidateStringList(Module.Name, Module.Dependencies, "Dependency name cannot be empty", Errors);
+
+        ValidatePlatformDependencies(Module.Name, "Linux", Module.PlatformDeps.Linux, Errors);
+        ValidatePlatformDependencies(Module.Name, "Windows", Module.PlatformDeps.Windows, Errors);
+        ValidatePlatformDependencies(Module.Name, "MacOS", Module.PlatformDeps.MacOS, Errors);
+
+        return Errors.Count > 0
+            ? ValidationResult.Failure(Errors)
+            : ValidationResult.Success();
     }
 
     /// <summary>
-    /// Pure semantic validator for module descriptors.
-    /// Validates build file structure and constraints without side effects.
+    /// Validates a collection of module descriptors.
     /// </summary>
-    public static class BuildFileValidator
+    public static ValidationResult ValidateAll(IEnumerable<ModuleDescriptor> Modules)
     {
+        var AllErrors = new List<ValidationError>();
 
-        /// <summary>
-        /// Validates a single module descriptor.
-        /// </summary>
-        public static ValidationResult Validate(ModuleDescriptor Module)
+        foreach (var Module in Modules)
         {
-            var Errors = new List<ValidationError>();
-
-            /** Validate module name */
-            if (string.IsNullOrWhiteSpace(Module.Name))
+            var Result = Validate(Module);
+            if (!Result.IsValid)
             {
-                Errors.Add(new ValidationError(Module.Name, "Module name cannot be empty"));
-            }
-
-            /** Validate module directory */
-            if (string.IsNullOrWhiteSpace(Module.Directory))
-            {
-                Errors.Add(new ValidationError(Module.Name, "Module directory cannot be empty"));
-            }
-
-            /** Validate module type */
-            if (!IsValidModuleType(Module.Type))
-            {
-                Errors.Add(new ValidationError(Module.Name, $"Invalid module type: {Module.Type}"));
-            }
-
-            /** Validate sources (allow empty for header-only modules) */
-            if (Module.Sources.Count == 0 && Module.PublicIncludes.Count == 0)
-            {
-                Errors.Add(new ValidationError(Module.Name, "Module must have at least one source file pattern or public include"));
-            }
-
-            for (int SourceIndex = 0; SourceIndex < Module.Sources.Count; SourceIndex++)
-            {
-                string Source = Module.Sources[SourceIndex];
-                if (string.IsNullOrWhiteSpace(Source))
+                foreach (var Error in Result.Errors)
                 {
-                    Errors.Add(new ValidationError(Module.Name, "Source pattern cannot be empty"));
-                }
-            }
-
-            /** Validate include paths */
-            for (int IncludeIndex = 0; IncludeIndex < Module.PublicIncludes.Count; IncludeIndex++)
-            {
-                string Include = Module.PublicIncludes[IncludeIndex];
-                if (string.IsNullOrWhiteSpace(Include))
-                {
-                    Errors.Add(new ValidationError(Module.Name, "Public include path cannot be empty"));
-                }
-            }
-
-            for (int IncludeIndex = 0; IncludeIndex < Module.PrivateIncludes.Count; IncludeIndex++)
-            {
-                string Include = Module.PrivateIncludes[IncludeIndex];
-                if (string.IsNullOrWhiteSpace(Include))
-                {
-                    Errors.Add(new ValidationError(Module.Name, "Private include path cannot be empty"));
-                }
-            }
-
-            /** Validate dependencies */
-            for (int DepIndex = 0; DepIndex < Module.Dependencies.Count; DepIndex++)
-            {
-                string Dep = Module.Dependencies[DepIndex];
-                if (string.IsNullOrWhiteSpace(Dep))
-                {
-                    Errors.Add(new ValidationError(Module.Name, "Dependency name cannot be empty"));
-                }
-            }
-
-            /** Validate platform dependencies */
-            ValidatePlatformDependencies(Module.Name, "Linux", Module.PlatformDeps.Linux, Errors);
-            ValidatePlatformDependencies(Module.Name, "Windows", Module.PlatformDeps.Windows, Errors);
-            ValidatePlatformDependencies(Module.Name, "MacOS", Module.PlatformDeps.MacOS, Errors);
-
-            return Errors.Count > 0
-                ? ValidationResult.Failure(Errors)
-                : ValidationResult.Success();
-        }
-
-        /// <summary>
-        /// Validates a collection of module descriptors.
-        /// </summary>
-        public static ValidationResult ValidateAll(IEnumerable<ModuleDescriptor> Modules)
-        {
-            var AllErrors = new List<ValidationError>();
-
-            foreach (var Module in Modules)
-            {
-                var Result = Validate(Module);
-                if (!Result.IsValid)
-                {
-                    foreach (var Error in Result.Errors)
-                    {
-                        AllErrors.Add(Error);
-                    }
-                }
-            }
-
-            return AllErrors.Count > 0
-                ? ValidationResult.Failure(AllErrors)
-                : ValidationResult.Success();
-        }
-
-        /// <summary>
-        /// Validates platform-specific dependencies.
-        /// </summary>
-        private static void ValidatePlatformDependencies(
-            string ModuleName,
-            string PlatformName,
-            IReadOnlyList<string> Dependencies,
-            List<ValidationError> Errors)
-        {
-            for (int DepIndex = 0; DepIndex < Dependencies.Count; DepIndex++)
-            {
-                string Dep = Dependencies[DepIndex];
-                if (string.IsNullOrWhiteSpace(Dep))
-                {
-                    Errors.Add(new ValidationError(ModuleName, $"{PlatformName} dependency name cannot be empty"));
+                    AllErrors.Add(Error);
                 }
             }
         }
 
-        /// <summary>
-        /// Checks if module type is valid.
-        /// </summary>
-        private static bool IsValidModuleType(ModuleType Type)
-        {
-            return Type == ModuleType.Executable
-                || Type == ModuleType.SharedLibrary
-                || Type == ModuleType.StaticLibrary;
-        }
+        return AllErrors.Count > 0
+            ? ValidationResult.Failure(AllErrors)
+            : ValidationResult.Success();
+    }
 
+    private static void ValidateStringList(
+        string ModuleName,
+        IReadOnlyList<string> Items,
+        string ErrorMessage,
+        List<ValidationError> Errors)
+    {
+        for (int Index = 0; Index < Items.Count; ++Index)
+        {
+            if (string.IsNullOrWhiteSpace(Items[Index]))
+            {
+                Errors.Add(new ValidationError(ModuleName, ErrorMessage));
+            }
+        }
+    }
+
+    private static void ValidatePlatformDependencies(
+        string ModuleName,
+        string PlatformName,
+        IReadOnlyList<string> Dependencies,
+        List<ValidationError> Errors)
+    {
+        for (int DepIndex = 0; DepIndex < Dependencies.Count; ++DepIndex)
+        {
+            if (string.IsNullOrWhiteSpace(Dependencies[DepIndex]))
+            {
+                Errors.Add(new ValidationError(ModuleName, $"{PlatformName} dependency name cannot be empty"));
+            }
+        }
+    }
+
+    private static bool IsValidModuleType(ModuleType Type)
+    {
+        return Type == ModuleType.Executable
+            || Type == ModuleType.SharedLibrary
+            || Type == ModuleType.StaticLibrary;
     }
 
 }
