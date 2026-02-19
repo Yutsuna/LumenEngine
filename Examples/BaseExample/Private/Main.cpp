7e47efd623be0ab016c9dd3d8b103736e37ddc8e
Leorevoir <leo.quinzler@epitech.eu>
2026-02-19 15:14:55 +0100

    Docs(Examples): Add BaseExample with Launch module included

--- Examples/BaseExample/Private/Main.cpp
+++ Examples/BaseExample/Private/Main.cpp
@@ -1,16 +1,1 @@
 #include "LaunchEngine.hpp"
-#include "LaunchEngineLoops.hpp"
-
-namespace LumenEngine
-{
-
-class GameInstance : public FEngineLoop
-{
-public:
-
-private:
-};
-
-} // namespace LumenEngine
-
-LUMEN_ENGINE_GAME( LumenEngine::GameInstance )
