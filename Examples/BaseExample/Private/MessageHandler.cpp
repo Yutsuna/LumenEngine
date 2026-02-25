e00a158af5568f352d008230ebec1a4c3f329f36
Leorevoir <leo.quinzler@epitech.eu>
2026-02-25 18:59:00 +0100

    Feat(BaseExample): Add MessageHandler

--- Examples/BaseExample/Private/MessageHandler.cpp
+++ Examples/BaseExample/Private/MessageHandler.cpp
@@ -7,16 +7,12 @@
 
 #include "LaunchEngineLoop.hpp"
 
-#include <iostream>
-
 void LumenEngine::FBaseExampleMessageHandler::OnWindowCloseRequested ( const TSharedRef<FGenericWindow> &InWindow )
 {
-    std::cout << "HELLOOOO" << std::endl;
     GEngineLoop.RequestExit( "Event: Window close requested" );
 }
 
 void LumenEngine::FBaseExampleMessageHandler::OnRequestExit ()
 {
-    std::cout << "Application exit requested." << std::endl;
     GEngineLoop.RequestExit( "Event: Application exit requested" );
 }
