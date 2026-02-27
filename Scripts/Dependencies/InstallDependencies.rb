0405add480eb52155091566b8e62629f072635e8
Leorevoir <leo.quinzler@epitech.eu>
2026-02-27 16:26:22 +0100

    Chore(Scripts): Add Vulkan Header

--- Scripts/Dependencies/InstallDependencies.rb
+++ Scripts/Dependencies/InstallDependencies.rb
@@ -4,49 +4,32 @@
 require_relative 'Source/Manager'
 
 
-# WINDOWS_CMAKE_BUILD_PARALLEL = [
-#   'cmake -S . -B build ' \
-#   '-G Ninja ' \
-#   '-DCMAKE_BUILD_TYPE=Release ' \
-#   '-DCMAKE_INSTALL_PREFIX=. ' \
-#   '-DCMAKE_C_COMPILER=clang-cl ' \
-#   '-DCMAKE_CXX_COMPILER=clang-cl',
-#   'cmake --build build --parallel',
-#   'cmake --install build'
-# ].freeze
-
-
-LINUX_CMAKE_BUILD_PARALLEL = [
+LINUX_CMAKE_BUILD_PARALLEL =[
     'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_INSTALL_LIBDIR=lib',
     'cmake --build build --parallel',
     'cmake --install build'
 ].freeze
 
 
-# MACOS_CMAKE_BUILD_PARALLEL = [
-#     'cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_INSTALL_LIBDIR=lib',
-#     'cmake --build build --parallel',
-#     'cmake --install build'
-# ].freeze
-
-
-DEPENDENCIES = [
+DEPENDENCIES =[
   {
     name: 'SDL3',
     version: '3.4.0',
     sources: {
-      # 'windows-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
-      # 'windows-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
       'linux-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
-      # 'macos-x64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz',
-      # 'macos-arm64': 'https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz'
     },
     build_commands: {
-      # 'windows-x64':        WINDOWS_CMAKE_BUILD_PARALLEL,
-      # 'windows-arm64':      WINDOWS_CMAKE_BUILD_PARALLEL,
       'linux-x64':          LINUX_CMAKE_BUILD_PARALLEL,
-      # 'macos-x64':          MACOS_CMAKE_BUILD_PARALLEL,
-      # 'macos-arm64':        MACOS_CMAKE_BUILD_PARALLEL
+    }
+  },
+  {
+    name: 'Vulkan',
+    version: '1.4.344',
+    sources: {
+      'linux-x64': 'https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.4.344.tar.gz'
+    },
+    build_commands: {
+      'linux-x64': LINUX_CMAKE_BUILD_PARALLEL
     }
   }
 ].freeze
