16fde70598e4ae6bfe952b12052a46491dede9f7
Leorevoir <leo.quinzler@epitech.eu>
2026-02-27 15:33:30 +0100

    Feat(LumenEngine): Add RHI

---
+++ LumenEngine/Source/RHI/Private/RHIDynamic.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file RHIDynamic.cpp
+ * @brief Implementation of the dynamic RHI interface
+ */
+
+#include "RHIDynamic.hpp"
+
+LumenEngine::RHI::IFRHIDynamic *LumenEngine::RHI::GDynamicRHI = nullptr;
---
+++ LumenEngine/Source/RHI/Public/RHIDynamic.hpp
@@ -0,0 +1,40 @@
+/**
+ * @file RHIDynamic.hpp
+ * @brief Interface for the dynamic RHI, which abstracts the underlying graphics API (e.g., Vulkan, DirectX, OpenGL).
+ */
+
+#pragma once
+
+#include "RHIViewport.hpp"
+
+#include "Container/SharedPtr.hpp"
+
+namespace LumenEngine
+{
+
+namespace RHI
+{
+
+    class LUMEN_ENGINE_API IFRHIDynamic
+    {
+    public:
+
+        virtual ~IFRHIDynamic () = default;
+
+    public:
+
+        /** Initializes the RHI (e.g., creates Instance, Physical Device, Logical Device) */
+        virtual void Init () = 0;
+
+        /** Cleans up the RHI resources */
+        virtual void Shutdown () = 0;
+
+        /** Creates a viewport linked to an OS-specific window handle */
+        virtual TSharedRef<IFRHIViewport> RHICreateViewport ( void *InWindowHandle, const UInt32 InWidth, const UInt32 InHeight ) = 0;
+    };
+
+    extern LUMEN_ENGINE_API IFRHIDynamic *GDynamicRHI;
+
+} // namespace RHI
+
+} // namespace LumenEngine
