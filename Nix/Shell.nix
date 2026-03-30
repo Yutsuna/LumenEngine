1c02ce8554d769f2a1620ce63508359b99f8b84a
Leorevoir <leo.quinzler@epitech.eu>
2026-03-30 17:37:45 +0200

    Chore(Nix): Add VMA export lib

--- Nix/Shell.nix
+++ Nix/Shell.nix
@@ -13,14 +13,15 @@ pkgs.mkShell {
     pkgs.vulkan-validation-layers
   ];
 
+    
   shellHook = ''
-    export LD_LIBRARY_PATH=${
-      pkgs.lib.makeLibraryPath [
+    export CPATH="${pkgs.vulkan-memory-allocator}/include:$CPATH"
+    export CPLUS_INCLUDE_PATH="${pkgs.vulkan-memory-allocator}/include:$CPLUS_INCLUDE_PATH"
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
         pkgs.vulkan-loader
         pkgs.sdl3
-      ]
-    }:$LD_LIBRARY_PATH
-
+      ]}:$LD_LIBRARY_PATH"
     export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
   '';
+
 }
