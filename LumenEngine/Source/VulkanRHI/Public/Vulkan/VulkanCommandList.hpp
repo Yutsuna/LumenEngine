b50f817d8be5865b888eccbdb58569ea6f06aa15
Leorevoir <leo.quinzler@epitech.eu>
2026-04-10 17:04:00 +0200

    Feat(VulkanRHI): Implement FVulkanCommandList for Vulkan command handling

---
+++ LumenEngine/Source/VulkanRHI/Public/Vulkan/VulkanCommandList.hpp
@@ -0,0 +1,58 @@
+
+/**
+ * @file VulkanCommandList.hpp
+ * @brief Vulkan implementation of IRHICommandList.
+ */
+
+#pragma once
+
+#include "Definitions.hpp"
+#include "RHI/RHICommandList.hpp"
+
+#include <vulkan/vulkan_core.h>
+
+namespace LumenEngine
+{
+
+namespace VulkanRHI
+{
+
+    class FVulkanRHI;
+
+    /**
+     * @class FVulkanCommandList
+     * @brief Translates generic RHI commands to Vulkan vkCmd calls.
+     */
+    class LUMEN_ENGINE_API FVulkanCommandList final : public RHI::IRHICommandList
+    {
+    public:
+
+        explicit FVulkanCommandList ( FVulkanRHI *InRHI ) : RHI( InRHI )
+        {
+        }
+        ~FVulkanCommandList () override = default;
+
+    public:
+
+        void BeginRendering ( const Float32 InClearColor[4] ) override;
+        void EndRendering () override;
+        void BindPipeline ( const RHI::FPipelineHandle InPipeline ) override;
+        void DrawMesh ( const RHI::FMeshHandle InMesh ) override;
+
+    public:
+
+        /** @brief Used internally to set the active Vulkan command buffer for this frame */
+        void SetActiveCommandBuffer ( VkCommandBuffer InCmd ) noexcept
+        {
+            CurrentCmd = InCmd;
+        }
+
+    private:
+
+        FVulkanRHI *RHI;
+        VkCommandBuffer CurrentCmd = VK_NULL_HANDLE;
+    };
+
+} // namespace VulkanRHI
+
+} // namespace LumenEngine
