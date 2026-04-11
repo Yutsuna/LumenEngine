33d6ac04d2523a3f0d11f6f74eedb52cd3f5d52a
Leorevoir <leo.quinzler@epitech.eu>
2026-04-11 17:05:57 +0200

    Style(Application): Update vertex definition and draw command

--- Examples/TriangleExample/Public/Application.hpp
+++ Examples/TriangleExample/Public/Application.hpp
@@ -51,7 +51,7 @@ private:
         const Maths::FVec3f Up     = { 0.0F, -1.0F, 0.0F };
 
         const Maths::FMatrix4x4f View       = Maths::FMatrix4x4f::LookAt( Eye, Target, Up );
-        const Maths::FMatrix4x4f Projection = Maths::FMatrix4x4f::Perspective( static_cast<Float32>( Maths::HalfPi ), 1280.0f / 720.0f, 0.1f, 100.0f );
+        const Maths::FMatrix4x4f Projection = Maths::FMatrix4x4f::Perspective( static_cast<Float32>( Maths::HalfPi ), 1280.0F / 720.0F, 0.1F, 100.0F );
     } Camera;
 
     Renderer::FRenderPacket RenderPacket;
