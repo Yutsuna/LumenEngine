dfef9e69b3e18050a93f1a79189ee9e7b87bb361
Leorevoir <leo.quinzler@epitech.eu>
2026-04-13 14:36:59 +0200

    Feat(Container): Add FAny

---
+++ LumenEngine/Source/Core/Public/Container/Any.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file Any.hpp
+ * @brief FAny class definition for type-erased storage in Lumen Engine.
+ */
+
+#pragma once
+
+#include <any>
+
+namespace LumenEngine
+{
+
+using FAny = std::any;
+
+}
