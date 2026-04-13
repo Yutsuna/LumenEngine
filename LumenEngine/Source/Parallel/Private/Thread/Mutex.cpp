c3706cc2101ac6c29704d36d48fc0713bae55dc6
Leorevoir <leo.quinzler@epitech.eu>
2026-04-13 14:40:53 +0200

    Fix(Mutex): Bad naming

--- LumenEngine/Source/Parallel/Private/Thread/Mutex.cpp
+++ LumenEngine/Source/Parallel/Private/Thread/Mutex.cpp
@@ -1,42 +1,42 @@
 /**
  * @file Mutex.cpp
-* @brief Implementation of the FMutex class for thread synchronization in Lumen Engine.
+ * @brief Implementation of the FMutex class for thread synchronization in Lumen Engine.
  */
 
 #include "Thread/Mutex.hpp"
 
 #include <atomic>
 
-void LumenEngine::FMutex::Lock() noexcept
+void LumenEngine::FMutex::Lock () noexcept
 {
-    if ( not MutexFlag.test_and_set(std::memory_order_::acquire))
+    if ( not MutexFlag.test_and_set( std::memory_order::acquire ) )
     {
         return;
     }
 
-    while (Flag.test(std::memory_order::relaxed))
+    while ( MutexFlag.test( std::memory_order::relaxed ) )
     {
-        Flag.wait(true, std::memory_order::relaxed);
+        MutexFlag.wait( true, std::memory_order::relaxed );
 
-        if ( not MutexFlag.test_and_set(std::memory_order_::acquire))
+        if ( not MutexFlag.test_and_set( std::memory_order::acquire ) )
         {
             return;
         }
     }
 }
 
-LumenEngine::Bool LumenEngine::FMutex::TryLock() noexcept
+LumenEngine::Bool LumenEngine::FMutex::TryLock () noexcept
 {
-    return not MutexFlag.test_and_set(std::memory_order_::acquire);
+    return not MutexFlag.test_and_set( std::memory_order::acquire );
 }
 
-void LumenEngine::FMutex::Unlock() noexcept
+void LumenEngine::FMutex::Unlock () noexcept
 {
-    MutexFlag.clear(std::memory_order_::release);
+    MutexFlag.clear( std::memory_order::release );
     MutexFlag.notify_one();
 }
 
-LumenEngine::Bool LumenEngine::FMutex::IsLocked() const noexcept
+LumenEngine::Bool LumenEngine::FMutex::IsLocked () const noexcept
 {
-    return MutexFlag.test(std::memory_order_::relaxed);
+    return MutexFlag.test( std::memory_order::relaxed );
 }
