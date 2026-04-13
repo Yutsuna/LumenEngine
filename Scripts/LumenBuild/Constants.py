7063019be9d7572ff7f82293ea06e35f5cb59245
Leorevoir <leo.quinzler@epitech.eu>
2026-04-13 17:25:17 +0200

    Chore(Constants): Update CLANG_FORMAT_EXTENSIONS to remove obsolete file types

--- Scripts/LumenBuild/Constants.py
+++ Scripts/LumenBuild/Constants.py
@@ -43,7 +43,7 @@ SANITIZER_ALIASES: dict[str, str] = {
 # Formatting
 # ---------------------------------------------------------------------------
 
-CLANG_FORMAT_EXTENSIONS = ("*.cpp", "*.hpp", "*.h", "*.cxx", "*.cc")
+CLANG_FORMAT_EXTENSIONS = ("*.cpp", "*.hpp", ".inl", ".frag", ".vert", ".comp")
 
 # ---------------------------------------------------------------------------
 # Colours
