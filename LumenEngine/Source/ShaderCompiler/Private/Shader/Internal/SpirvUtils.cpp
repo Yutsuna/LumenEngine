9329572e24239f7aafe2d1c063db64737300fb50
Leorevoir <leo.quinzler@epitech.eu>
2026-04-27 19:10:42 +0200

    Feat(SpirvUtils): Enhance SPIR-V utility functions with validation and disassembly capabilities

--- LumenEngine/Source/ShaderCompiler/Private/Shader/Internal/SpirvUtils.cpp
+++ LumenEngine/Source/ShaderCompiler/Private/Shader/Internal/SpirvUtils.cpp
@@ -1,6 +1,38 @@
 /**
  * @file SpirvUtils.cpp
- * @brief Implementation of the SpirvUtils functions.
+ * @brief Implementation of the SPIR-V utility backend.
  */
 
 #include "Shader/Internal/SpirvUtils.hpp"
+
+#include <format>
+#include <spirv-tools/libspirv.hpp>
+
+LumenEngine::FString LumenEngine::Internal::FSpirvUtils::Disassemble ( const FSpirVBlob &InSpirV ) noexcept
+{
+    spvtools::SpirvTools Tools( SPV_ENV_VULKAN_1_3 );
+    FString Assembly;
+
+    if ( Tools.Disassemble( InSpirV.data(), InSpirV.size(), &Assembly ) )
+    {
+        return Assembly;
+    }
+
+    return "";
+}
+
+LumenEngine::FString LumenEngine::Internal::FSpirvUtils::Validate ( const FSpirVBlob &InSpirV ) noexcept
+{
+    spvtools::SpirvTools Tools( SPV_ENV_VULKAN_1_3 );
+    FString ErrorMsg;
+
+    Tools.SetMessageConsumer( [&ErrorMsg] ( spv_message_level_t /* Level */, const char * /* Source */, const spv_position_t &Pos, const char *Msg )
+                              { ErrorMsg += std::format( "Validation error at line {}: {}\n", Pos.line, Msg ); } );
+
+    if ( Tools.Validate( InSpirV.data(), InSpirV.size() ) )
+    {
+        return "";
+    }
+
+    return ErrorMsg;
+}
