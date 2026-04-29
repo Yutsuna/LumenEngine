94f0edc5528ee81a2c1187ce600048c053cf95e1
Leorevoir <leo.quinzler@epitech.eu>
2026-04-29 16:43:15 +0200

    Chore(Hash): Add ComputeRaw

--- LumenEngine/Source/Core/Private/Hash/Hash.cpp
+++ LumenEngine/Source/Core/Private/Hash/Hash.cpp
@@ -4,3 +4,11 @@
  */
 
 #include "Hash/Hash.hpp"
+
+LumenEngine::FHashValue LumenEngine::Hash::ComputeRaw ( const void *InData, USize InSize, FHashValue InSeed ) noexcept
+{
+    FFnv1a64 Algorithm( InSeed );
+
+    Algorithm.Write( InData, InSize );
+    return Algorithm.Digest();
+}
