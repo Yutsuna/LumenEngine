5cb9d556843e5a4afddc537231f96d83c546b87a
Leorevoir <leo.quinzler@epitech.eu>
2026-04-30 00:15:43 +0200

    Refactor(Optional): Add LUMEN_OPTIONAL_ERROR macros to allow custom error handling

--- LumenEngine/Source/Core/Public/Container/Optional.hpp
+++ LumenEngine/Source/Core/Public/Container/Optional.hpp
@@ -12,13 +12,23 @@ namespace LumenEngine
 
 template <typename Type> using TOptional = std::optional<Type>;
 
-#define LUMEN_OPTIONAL( OptionalValue )                                                                                                                                  \
+/**
+ * @macro LUMEN_OPTIONAL_ERROR
+ * @brief Checks if the optional value has a value, and if not, returns the specified error.
+ */
+#define LUMEN_OPTIONAL_ERROR( OptionalValue, Error )                                                                                                                     \
     do                                                                                                                                                                   \
     {                                                                                                                                                                    \
         if ( not( OptionalValue ).has_value() )                                                                                                                          \
         {                                                                                                                                                                \
-            return std::nullopt;                                                                                                                                         \
+            return Error;                                                                                                                                                \
         }                                                                                                                                                                \
     } while ( false )
+
+/**
+ * @macro LUMEN_OPTIONAL
+ * @brief Checks if the optional value has a value, and if not, returns std::nullopt.
+ */
+#define LUMEN_OPTIONAL( OptionalValue ) LUMEN_OPTIONAL_ERROR( OptionalValue, std::nullopt )
 
 } // namespace LumenEngine
