130927cfb5437e22cf9e6bcaf03d54cfcba36c64
Leorevoir <leo.quinzler@epitech.eu>
2026-04-30 11:43:33 +0200

    Chore(Compiler): Add LumenCompilerTypes

---
+++ LumenEngine/Source/Compiler/Private/LumenCompiler/LumenCompilerTypes.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file LumenCompilerTypes.cpp
+ * @brief Implementation of LumenCompilerTypes
+ */
+
+#include "LumenCompiler/LumenCompilerTypes.hpp"
+
+const LumenEngine::FLogCategory LumenEngine::Compiler::LogLumenCompiler( "LumenCompiler" );
